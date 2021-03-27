eba5b0630192e999d6a51e10d721468a4499d7dd
Joao Porfirio <joaoprf54@gmail.com>
2021-03-27 17:21:24 -0300

    Getters and Setters Implementation

--- bytebank/ContaCorrente.js
+++ bytebank/ContaCorrente.js
@@ -1,6 +1,23 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente{
   agencia;
+  _cliente;
+
+  set cliente(novoValor){
+    if(novoValor instanceof Cliente)
+      this._cliente = novoValor;
+
+  }
+
+  get cliente(){
+    return this._cliente;
+  }
   _saldo = 0;
+
+  get saldo(){
+    return this._saldo;
+  }
 
   sacar(valor){
       if(this._saldo >= valor){
@@ -13,6 +30,10 @@ export class ContaCorrente{
       if(valor <= 0) return;
       this._saldo += valor;    
       
+  }
 
+  transferir(valor, conta){
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
   }
 }
